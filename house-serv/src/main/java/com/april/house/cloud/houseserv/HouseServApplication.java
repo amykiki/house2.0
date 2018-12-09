5a6b16bb61be465509d11a1e9d98fc9fc0cf69e0
amykiki <staramy_2005@126.com>
2018-12-09 20:03:09 +0800

    init house2.0

---
+++ house-serv/src/main/java/com/april/house/cloud/houseserv/HouseServApplication.java
@@ -0,0 +1,12 @@
+package com.april.house.cloud.houseserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseServApplication.class, args);
+    }
+}
---
+++ house-serv/src/test/java/com/april/house/cloud/houseserv/HouseServApplicationTests.java
@@ -0,0 +1,16 @@
+package com.april.house.cloud.houseserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HouseServApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
