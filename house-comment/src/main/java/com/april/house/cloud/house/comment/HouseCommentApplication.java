2811a314c1c0f1b7782aef31f910e88d965cae7c
amykiki <staramy_2005@126.com>
2018-12-21 23:23:01 +0800

    update repo

---
+++ house-comment/src/main/java/com/april/house/cloud/house/comment/HouseCommentApplication.java
@@ -0,0 +1,14 @@
+package com.april.house.cloud.house.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseCommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseCommentApplication.class, args);
+    }
+
+}
+
